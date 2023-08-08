64a3fb3a86642f699738ea95c0347f3aa184a281
Marlo-Ong <moon.ongkingco@gmail.com>
2023-08-08 15:16:03 -0700

    Prevented making duplicate weeds on one tile

--- Kuddelmuddel/Assets/Scripts/Swipe Modes/WeedMaker.cs
+++ Kuddelmuddel/Assets/Scripts/Swipe Modes/WeedMaker.cs
@@ -7,6 +7,7 @@ public class WeedMaker : MonoBehaviour
 {
     [SerializeField] public GameObject weedPrefab;
     [SerializeField] private Tilemap canvas;
+    public Dictionary<Vector3Int, GameObject> weedLocations = new Dictionary<Vector3Int, GameObject>(); // cell, weed reference
 
     private TileGetter tg;
 
@@ -17,12 +18,18 @@ public class WeedMaker : MonoBehaviour
     public void MakerUpdate() {
         if (Input.touchCount > 0){
             tg.TouchUpdate(canvas, Input.GetTouch(0).position);
-            MakeWeed();
-            //newWeed.transform.parent = UnityEngine.GameObject.Find("Above Ground").transform;
-        }
-    }
 
-    public void MakeWeed(){
-        GameObject newWeed = Instantiate(weedPrefab, tg.lastWorldPt, Quaternion.identity);
+            if (!(weedLocations.ContainsKey(tg.lastCell))){
+                GameObject newWeed = Instantiate(weedPrefab, tg.lastWorldPt, Quaternion.identity);
+                newWeed.transform.parent = UnityEngine.GameObject.Find("Above Ground").transform;
+                newWeed.name = "Weed" + weedLocations.Count;
+
+                print("Added weed at " + tg.lastCell);
+                weedLocations.Add(tg.lastCell, newWeed);
+            }
+            else if (Input.GetTouch(0).phase == TouchPhase.Began){
+                print("A weed already exists at " + tg.lastCell);
+            }
+        }
     }
 }
