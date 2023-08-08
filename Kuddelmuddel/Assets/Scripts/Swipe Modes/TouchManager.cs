5a341da99bac27af75cb6014eeedfdbde0b13a4d
Marlo-Ong <moon.ongkingco@gmail.com>
2023-08-08 13:12:34 -0700

    Added pinch zoom functionality in swipe modes

--- Kuddelmuddel/Assets/Scripts/Swipe Modes/TouchManager.cs
+++ Kuddelmuddel/Assets/Scripts/Swipe Modes/TouchManager.cs
@@ -2,28 +2,37 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.Tilemaps;
 
 public class TouchManger : MonoBehaviour
 {
     private Navigation nv;
     private WeedMaker wm;
     //public WeedEraser we;
-    //public PinchZoom pz;
+    private PinchZoom pz;
+    private TileGetter tg;
 
     [SerializeField] private int mode;
+    [SerializeField] private GameObject trail;
+    [SerializeField] private Tilemap tilemap;
 
     void Start(){
         GameObject self = GameObject.Find("Touch Manager");
         nv = self.GetComponent<Navigation>();
         wm = self.GetComponent<WeedMaker>();
         //we = self.GetComponent<WeedEraser>();
-        //pz = self.GetComponent<PinchZoom>();
+        pz = self.GetComponent<PinchZoom>();
+        tg = self.GetComponent<TileGetter>();
     }
 
     void Update()
     {
         if(Input.touchCount > 0)
         {
+            tg.TouchUpdate(tilemap, Input.GetTouch(0).position);
+            trail.SetActive(true);
+            trail.transform.position = tg.lastWorldPt;
+
             switch (mode){
                 case 0:
                     nv.NavUpdate();
@@ -31,10 +40,12 @@ public class TouchManger : MonoBehaviour
                 case 1:
                     wm.MakerUpdate();
                     break;
-                // case 2:
-                //     we.EraseUpdate();
                 // case 3:
-                //     pz.ZoomUpdate();
+                //     we.EraseUpdate();
+                //        break;
+                case 2:
+                    pz.ZoomUpdate();
+                    break;
             }
         }
     }
