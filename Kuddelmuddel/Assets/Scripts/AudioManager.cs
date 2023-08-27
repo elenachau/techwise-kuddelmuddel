5a412b56cbb972f390849427134281138741527d
Marlo-Ong <moon.ongkingco@gmail.com>
2023-08-27 12:11:16 -0700

    Add volume slider functionality

--- Kuddelmuddel/Assets/Scripts/AudioManager.cs
+++ Kuddelmuddel/Assets/Scripts/AudioManager.cs
@@ -1,10 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class AudioManager : MonoBehaviour
 {
     public static AudioManager Instance;
+    [SerializeField] private Slider musicSlider, effectsSlider;
     [SerializeField] private AudioSource musicSource, effectsSource;
 
     void Awake() {
@@ -19,5 +21,13 @@ public class AudioManager : MonoBehaviour
 
     public void PlaySoundEffect(AudioClip clip) {
         effectsSource.PlayOneShot(clip);
+    }
+
+    public void ChangeMusicVolume() {
+        musicSource.volume = musicSlider.value;
+    }
+
+    public void ChangeEffectsVolume() {
+        effectsSource.volume = effectsSlider.value;
     }
 }
